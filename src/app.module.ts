002e3e7dea7fa0514fd23dd81daf8e3e21e21ddb
foo-val <tedxcdj@kakao.com>
2025-04-03 11:03:41 +0900

    order module import 문 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { AuthsModule } from './auths/auths.module';
 import { JwtAuthGuard } from './common/guards/jwt-auth.guard';
 import { RolesGuard } from './common/guards/roles.guard';
 import { envConfig } from './config/env.config';
-import { OrderModule } from './order/\border.module';
+import { OrderModule } from './order/order.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ProductPolicyModule } from './product/product-policy.module';
 import { ProductModule } from './product/product.module';
